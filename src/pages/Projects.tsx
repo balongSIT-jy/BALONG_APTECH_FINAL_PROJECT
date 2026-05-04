30f5067ebbe710d65d9b3c1b9b7eda78da051395
Jyra Balong <20256525@s.ubaguio.edu>
2026-05-04 10:08:55 +0800

    changed something

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -8,6 +8,11 @@ function Projects() {
         <div className="col-md-4">
           <div className="card p-3 shadow-sm">
             <h5>Project 1</h5>
+            <a href="https://balongsit-jy.github.io/MG_LAB5_BALONG/"
+              target="_blank"
+              rel="mg lab 5"
+              className="btn btn-primary"
+            >MG LAB 5</a>
             <p>
             </p>
           </div>
@@ -16,14 +21,22 @@ function Projects() {
         <div className="col-md-4">
           <div className="card p-3 shadow-sm">
             <h5>Project 2</h5>
-            <p></p>
+            <a href="https://balongsit-jy.github.io/MG_LAB4_BALONG/"
+              target="_blank"
+              rel="mg lab 4"
+              className="btn btn-primary"
+            >MG LAB 4</a>
           </div>
         </div>
 
         <div className="col-md-4">
           <div className="card p-3 shadow-sm">
             <h5>Project 3</h5>
-            <p></p>
+            <a href="https://balongsit-jy.github.io/aptech_final_contact_with_gmail/"
+              target="_blank"
+              rel="contact with gmail"
+              className="btn btn-primary"
+            >Contact with Gmail</a>
           </div>
         </div>
       </div>
