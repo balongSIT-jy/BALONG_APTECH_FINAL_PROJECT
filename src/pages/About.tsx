1b559bc74de34d5df7b6e1125787bfc98d28115f
20241392@s.ubaguio.edu <20241392@s.ubaguio.edu>
2026-05-11 05:45:31 +0800

    made some changes

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,14 +1,26 @@
-
 function About() {
   return (
-    <section id="about" className="bg-dark text-white text-center py-5">
+    <section 
+      id="about" 
+      // Changed text-white to text-dark
+      className="text-dark text-center py-5" 
+      style={{ backgroundColor: "white" }} 
+    >
       <div className="container">
-        <h1 className="display-4 fw-bold">About Me</h1>
-        <p className="lead">
-        Hi, I’m a student developer passionate about building simple, responsive, and user-friendly applications. I enjoy learning new technologies, especially in web development, and I’m always looking for ways to improve my skills and create meaningful projects.
-      </p>
+       
+        <h2 className="fw-bold mb-3">About Me</h2>
+        
+        <div className="row justify-content-center">
+          <div className="col-md-7">
+            <p style={{ fontSize: "1rem", lineHeight: "1.6" }}>
+              Hi, I’m a student developer passionate about building simple, responsive, 
+              and user-friendly applications. I enjoy learning new technologies, 
+              especially in web development, and I’m always looking for ways to 
+              improve my skills and create meaningful projects.
+            </p>
+          </div>
+        </div>
       </div>
-      
     </section>
   );
 }
